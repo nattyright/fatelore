e2e59202e43504948fb9d3a68623e1cabd5d77c0
nat <hichoshi@gmail.com>
2021-06-29 00:29:24 +0800

    enable local storage

--- _faterpTool/js/common/util.js
+++ _faterpTool/js/common/util.js
@@ -31,3 +31,113 @@ async function fetchAsync (url) {
 }
 
 
+
+
+
+
+var idsToSave = ['title', 
+            'sideName', 
+            'sideClass', 
+            'sideAlias', 
+            'sideGender', 
+            'sideBorn', 
+            'sideDied', 
+            'sideHeight', 
+            'sideWeight', 
+            'sideHeightUnit2', 
+            'sideNationality', 
+            'sideAlignment', 
+            'sideWeapon', 
+            'sideLikes', 
+            'sideDislikes', 
+            'sideStr', 
+            'sideEnd', 
+            'sideAgi', 
+            'sideMan', 
+            'sideLuc', 
+            'sideNP', 
+            'personality', 
+            'backstory', 
+            'otherInfo', 
+            'description', 
+            'weapon', 
+            'np1Name', 
+            'np1Chant', 
+            'np1Classifier', 
+            'np1Rank', 
+            'np1Range', 
+            'np1Targets', 
+            'np1Description', 
+            'np1Effect', 
+            'np2Name', 
+            'np2Chant', 
+            'np2Classifier', 
+            'np2Rank', 
+            'np2Range', 
+            'np2Targets', 
+            'np2Description', 
+            'np2Effect', 
+            'cSkill1Name', 
+            'cSkill1Rank', 
+            'cSkill1Description', 
+            'cSkill2Name', 
+            'cSkill2Rank', 
+            'cSkill2Description', 
+            'cSkill3Name', 
+            'cSkill3Rank', 
+            'cSkill3Description', 
+            'pSkill1Name', 
+            'pSkill1Rank', 
+            'pSkill1Description', 
+            'pSkill1Example', 
+            'pSkill2Name', 
+            'pSkill2Rank', 
+            'pSkill2Description', 
+            'pSkill2Example', 
+            'pSkill3Name', 
+            'pSkill3Rank', 
+            'pSkill3Description', 
+            'pSkill3Example'];
+
+
+
+/*
+ * Cache info in local storage
+ */
+function saveFormData(divId) {
+    if (typeof(Storage) !== "undefined") {
+      // Store
+      localStorage.setItem(divId, document.getElementById(divId).value);
+    } else {
+      document.getElementById("divId").value = "Sorry, your browser does not support Web Storage...";
+    }
+}
+/*
+ * Load info from local storage
+ */
+function loadFormData(divId) {
+    if (localStorage.getItem(divId) != null && document.getElementById(divId) != null) {
+        document.getElementById(divId).value = localStorage.getItem(divId);
+    }
+}
+
+
+$( document ).ready(function() {
+    // retrieve cache data, if any
+    for (var i = 0; i < idsToSave.length; i++) {
+        loadFormData(idsToSave[i]);
+    }
+});
+
+
+var saveToLocalTimeoutId;
+$('form input, form textarea, form select, form button, form option').on('input propertychange change', function() {
+    //console.log(this.value);
+    var toSaveId = $(this).attr('id');
+
+    clearTimeout(saveToLocalTimeoutId);
+    timeoutId = setTimeout(function() {
+        // Runs 1 second (1000 ms) after the last change    
+        saveFormData(toSaveId);
+    }, 1000);
+});
