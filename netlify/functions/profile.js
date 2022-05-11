52c5f6e1e24e8d69a60e05fd569dff61defe29d7
nat <hichoshi@gmail.com>
2022-05-11 20:54:11 +0800

    test node

---
+++ netlify/functions/profile.js
@@ -0,0 +1,63 @@
+
+// mongodb
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = process.env.MONGODB_URI;
+const DB_NAME = 'fgogacha';
+
+
+let cachedDb = null;
+
+const connectToDatabase = async (uri) => {
+
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = client.db(DB_NAME);
+
+  return cachedDb;
+};
+
+
+const queryDatabase = async (db) => {
+  const pokemon = await db.collection("fgogacha").find({}).toArray();
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(pokemon),
+  };
+};
+
+const pushToDatabase = async (db, data) => {
+  const pokemonData = {
+    name: data.name,
+    number: data.number,
+  };
+
+  if (pokemonData.name && pokemonData.number) {
+    await db.collection("pokemon").insertMany([data]);
+    return { statusCode: 201 };
+  } else {
+    return { statusCode: 422 };
+  }
+};
+
+module.exports.handler = async (event, context) => {
+  // otherwise the connection will never complete, since
+  // we keep the DB connection alive
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(MONGODB_URI);
+
+  switch (event.httpMethod) {
+    case "GET":
+      return queryDatabase(db);
+    case "POST":
+      return pushToDatabase(db, JSON.parse(event.body));
+    default:
+      return { statusCode: 400 };
+  }
+};
